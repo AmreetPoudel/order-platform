4d11dbd8008bbf8c5dbc0d9383a4ea291ac49a0d
amrit poudel <aamreet12345@gmail.com>
2026-07-17 21:55:24 +0545

    route table added route

--- infra/modules/route_table/main.tf
+++ infra/modules/route_table/main.tf
@@ -1,7 +1,14 @@
-resource "aws_internet_gateway" "order_platform_gw" {
-  vpc_id = aws_vpc.main.id
+resource "aws_route_table" "order_platform_route_table" {
+  vpc_id = var.order_platform_vpc_id
+
+  route {
+  cidr_block = "0.0.0.0/0"       
+  gateway_id = var.order_platform_gw_id  
+  }
+
+ 
 
   tags = {
-    Name = "main"
+    Name = "order_platform_route_table"
   }
 }
--- infra/modules/route_table/variable.tf
+++ infra/modules/route_table/variable.tf
@@ -2,3 +2,10 @@ variable "order_platform_vpc_id" {
   type        = string
   description = "VPC ID this security group belongs to"
 }
+
+
+
+variable "order_platform_gw_id" {
+  type        = string
+  description = "internet gateway id"
+}
