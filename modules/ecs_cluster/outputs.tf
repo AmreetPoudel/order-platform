cc4ab6055a83ecc6413e907e06e995a8633a05b2
amrit poudel <aamreet12345@gmail.com>
2026-08-11 23:59:44 +0545

    bugfix: typo

--- modules/ecs_cluster/outputs.tf
+++ modules/ecs_cluster/outputs.tf
@@ -1,7 +1,7 @@
 output "cluster_id" {
-  value = aws_ecs_cluster.order_platform.id
+  value = aws_ecs_cluster.order_platform_ecs_cluster.id
 }
 
 output "cluster_name" {
-  value = aws_ecs_cluster.order_platform.name
+  value = aws_ecs_cluster.order_platform_ecs_cluster.name
 }
