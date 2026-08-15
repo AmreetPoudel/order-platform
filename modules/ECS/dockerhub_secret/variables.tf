3f40fd97093b3e611f425b96bd64f80b2df98257
amrit poudel <aamreet12345@gmail.com>
2026-08-15 10:39:15 +0545

    ALB readded

--- modules/ECS/dockerhub_secret/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "ssm_username_path" {
-  type = string
-}
-variable "ssm_token_path" {
-  type = string
-}
-variable "secret_name" {
-  type = string
-}
--- modules/ECS/ecs_cluster/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "cluster_id" {
-  value = aws_ecs_cluster.order_platform_ecs_cluster.id
-}
-
-output "cluster_name" {
-  value = aws_ecs_cluster.order_platform_ecs_cluster.name
-}
